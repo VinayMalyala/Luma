2bee1e4fe5a941ab4786a4093605cabf46cf2c3f
Vinay Malyala <srivinay1432@gmail.com>
2024-10-28 23:37:08 +0530

    add scroll behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './components/Main/Home';
+import Home from './components/Home/Home';
 import SideBar from './components/SideBar/SideBar';
 
 export default function App(){
